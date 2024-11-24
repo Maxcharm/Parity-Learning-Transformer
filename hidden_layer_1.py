482ded42b0d1ec6a30d3766418f342357e06cba9
Yaomengxi Han <hanyaomengxi@hotmail.com>
2024-11-24 23:14:51 +0100

    add visualisation with heatmaps

--- hidden_layer_1.py
+++ hidden_layer_1.py
@@ -6,6 +6,10 @@ import random
 import math
 from hard_attention import Attention
 import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
 # %%
 
 
@@ -23,11 +27,11 @@ def simple_embedding(x):
 
 def data_generator(
         num_data: int,
-        k: int = 3,
-        n: int = 16,
+        k: int = 4,
+        n: int = 20,
         ):
-    # parity_bits = random.sample(range(n), k)
-    parity_bits = [2, 4, 15]
+    parity_bits = random.sample(range(n), k)
+    # parity_bits = [2, 4, 15]
     num = 2 ** n
     x = torch.zeros((num, n), dtype=torch.float32)
     for i in range(num):
@@ -44,18 +48,17 @@ def data_generator(
 
 
 # %%
-def visualize_attention(heads, true_bits, sample):
+def collect_attention(heads, true_bits, sample):
     length = sample.shape[1]
     v0 = torch.zeros(length)
     v0[0] = 1
-    print(f"the true bits are {true_bits}.")
-    for i, head in enumerate(heads):
+    weights = []
+    for head in heads:
         Av0 = torch.matmul(v0, head.A)
         scores = torch.matmul(sample, Av0.unsqueeze(-1)).squeeze(-1)
-        attention_weights = F.softmax(scores, dim=-1) 
-        maximum_position = torch.argmax(scores, dim=-1)
-        print(f"The {i+1}-th head attend: {maximum_position}-th bit with score {attention_weights[maximum_position]:.2f}.")
-    print("--------------------------------")
+        attention_weights = F.softmax(scores, dim=-1)
+        weights.append(attention_weights)
+    return weights
 
 
 # %%
@@ -99,8 +102,26 @@ class parity_NN(nn.Module):
         attention_vectors = torch.concat(attention_vectors, dim=1)
         return self.network(attention_vectors)
 # %%
+def visualize_weights(weights, true_bits, length, num_epochs):
+    weights = np.array(weights)
+    num_heads = weights.shape[1]
+    fig, axes = plt.subplots(1, num_heads, figsize=(15, 5), sharey=True)
+    for head_idx in range(num_heads):
+        ax = axes[head_idx]
+        head_scores = weights[:, head_idx, :]
+        heatmap = ax.imshow(head_scores, aspect="auto", cmap="viridis",
+                            origin="lower", extent=[0.5, length - 0.5, 0, num_epochs])
+        ax.set_title(f"Head {head_idx + 1}")
+        ax.set_xlabel("Attention score for each position")
+        ax.set_ylabel("Epoch")
+        ax.set_xticks(true_bits)
+        ax.set_xticklabels([f"{pos}" for pos in true_bits], rotation=45, ha="right")
+        cbar = fig.colorbar(heatmap, ax=ax, orientation="vertical")
+        cbar.set_label("Attention Score")
+    plt.tight_layout()
+    plt.show()
 
-
+# %%
 def test(model, x, y):
     pred = model(x)
     predicted_classes = (pred >= 0.5).float()
@@ -110,38 +131,44 @@ def test(model, x, y):
 
 
 if __name__ == "__main__":
-    number_of_data = 50000
+    number_of_data = int(2**20 * 0.8)
     k = 3
-    epochs = 40000
+    epochs = 30
+    batch_size = 8000
     loss_fn = HingeLoss(task="binary")
     # loss_fn = nn.MSELoss()
     x, y, data, label, bits = data_generator(number_of_data, k)
+    dataset = TensorDataset(data, label)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     parity_network = parity_NN(k)
     attention_params = (
         list(parity_network.attention_heads[0].parameters()) +
         list(parity_network.attention_heads[1].parameters()) +
         list(parity_network.attention_heads[2].parameters())
+        # list(parity_network.attention_heads[3].parameters())
     )
-    ffnn_params = list(parity_network.network.parameters())
+    total_weights = []
     with torch.no_grad():
-        visualize_attention(parity_network.attention_heads, bits, data[0])
+        total_weights.append(collect_attention(parity_network.attention_heads, bits, data[0]))
     optimizer = torch.optim.Adam(attention_params, lr=8e-2)
     parity_network.train()
+    
     for i in range(epochs):
-        pred = parity_network(data)
-        loss = loss_fn(pred, label)
-
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        loss = loss.item()
-        if i % 2000 == 0:
-            print(f"epoch: {i}, loss: {loss:>7f}.")
-            with torch.no_grad():
-                visualize_attention(parity_network.attention_heads, bits, data[0])
-            # for param in attention_params:
-            #     print(param)
+        total_loss = 0
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            pred = parity_network(inputs)
+            loss = loss_fn(pred, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        # if i % 2 == 0:
+        print(f"epoch: {i}, loss: {loss:>7f}.")
+        with torch.no_grad():
+            total_weights.append(collect_attention(parity_network.attention_heads, bits, data[0]))
+    with open("weights_test.pkl", "wb") as f:
+        pickle.dump(total_weights, f)
+    visualize_weights(weights=total_weights, true_bits=bits, length=20, num_epochs=epochs)
     parity_network.eval()
-    print(test(parity_network, x, y))
+    print(test(parity_network, x[:2000], y[:2000]))
 # %%
